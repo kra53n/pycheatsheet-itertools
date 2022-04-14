f710523757776fe7260e2f9b6be57df41bb5d753
Krai53n <greasha46@gmail.com>
2022-04-14 16:38:57 +0300

    del enters

--- examples/islice.py
+++ examples/islice.py
@@ -4,17 +4,13 @@ from itertools import islice
 lst = [*range(100)]
 
 start_time = monotonic()
-
 print('Используем стандартный срез')
 print('Список от 0 до 50: ', lst[:50])
-
 print(f'Прошло {monotonic() - start_time} сек.')
 print()
 
 
 start_time = monotonic()
-
 print('Используем itertools.islice')
 print('Список от 0 до 50: ', [*islice(lst, 0, 50)])
-
 print(f'Прошло {monotonic() - start_time} сек.')
