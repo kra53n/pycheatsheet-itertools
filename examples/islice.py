e750acaa41d93f83f66a11ba3872a0b6d71469ea
Krai53n <greasha46@gmail.com>
2022-04-14 16:37:04 +0300

    add comparing speed of islice to standart sliceing

---
+++ examples/islice.py
@@ -0,0 +1,20 @@
+from time import monotonic
+from itertools import islice
+
+lst = [*range(100)]
+
+start_time = monotonic()
+
+print('Используем стандартный срез')
+print('Список от 0 до 50: ', lst[:50])
+
+print(f'Прошло {monotonic() - start_time} сек.')
+print()
+
+
+start_time = monotonic()
+
+print('Используем itertools.islice')
+print('Список от 0 до 50: ', [*islice(lst, 0, 50)])
+
+print(f'Прошло {monotonic() - start_time} сек.')
